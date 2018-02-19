2db48803c421205cc9aad4f8a7b86e1b4163511e
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-19 10:48:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ element.innerHTML = "I'm still getting Hungrier! Rawwrrrr...";
 
 //Moves the image
 var img = document.getElementById("madi");
-img.onClick = function(){
+img.onclick = function(){
     img.style.marginLeft = '100px';
 };
