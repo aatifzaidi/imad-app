7e6aeb4fbe26e41e5feb9fc0e7f76efa24d8bbcf
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-19 10:40:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
-
+//For Changing the text of the Home page Div
 var element = document.getElementById("main-text");
 element.innerHTML = "I'm still getting Hungrier! Rawwrrrr...";
+
+//Moves the image
+var img = document.getElementById("img");
+img.onClick = function(){
+    img.style.marginLeft = '100px';
+};
