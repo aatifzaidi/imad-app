8ccfcaa6013932aea15b417dec6e948cb0832173
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-24 15:53:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,9 @@
-console.log('Loaded!');
+var button = document.getElementById("counter");
+var counter =0;
 
-//For Changing the text of the Home page Div
-var element = document.getElementById("main-text");
-element.innerHTML = "I'm still getting Hungrier! Rawwrrrr...";
-
-//Moves the image
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+button.onclick = function(){
+    
+    counter++;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
