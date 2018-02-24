4e2634e0967b7b00410e1466d0f72d457be06556
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-24 17:06:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,24 @@
 var button = document.getElementById("counter");
-var counter =0;
 
 button.onclick = function(){
     
-    counter++;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable 
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest.Done){
+           if(request.status === 200){
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+               
+           }
+       } 
+    };
+    
+    //Make the request
+    
+    request.open('GET',"http://aatifsuntech.imad.hasura-app.io/",true);
+    request.send(null);
 };
