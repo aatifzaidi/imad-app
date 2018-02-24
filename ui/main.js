2228e1ff53035c70859e33fb4ec262b2df181222
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-24 17:53:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,18 @@ button.onclick = function(){
     request.open('GET','http://aatifsuntech.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list += '<li>'+names[i]+'</li>';
+    }
+    var ol = document.getElementById('namelist');
+    ol.innerHTML = 'list';
+};
