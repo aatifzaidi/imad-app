6551de80e08ddb73133936d456e411f30f0cd8b7
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-24 19:39:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(){
+app.get('/submit-name/:name', function(req,res){
    var name = req.params.name;
    
    names.push(name);
