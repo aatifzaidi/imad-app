76871da60f6f1aab5582aee6652417cbf81656ff
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-26 22:16:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user:'aatifsuntech'
+    database:'aatifsuntech'
+    host:'db.imad.hasura-app.io'
+    port:'5432'
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -132,6 +141,21 @@ function createTemplate(data){
     `;
     return htmlTemplate;
 }
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res){
+    //make a select response
+    //return a response with the results
+    pool.query('SELECT * FROM emp',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
