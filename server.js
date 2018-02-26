ebeb798a96517b2ef1368c5a2c97fef6327a7788
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-26 22:20:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user:'aatifsuntech'
-    database:'aatifsuntech'
-    host:'db.imad.hasura-app.io'
-    port:'5432'
+    user:'aatifsuntech',
+    database:'aatifsuntech',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
     password:process.env.DB_PASSWORD
 };
 
