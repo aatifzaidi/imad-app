b5e5988b94a94b964988f2615818938458f7031a
Aatif Zaidi <aatifsuntech@gmail.com>
2018-02-26 23:13:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database:'aatifsuntech',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-aatifsuntech-43713'
 };
 
 var app = express();
