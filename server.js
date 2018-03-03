6b9c8b2bc3fae812f8d9471abc1ce5e434cccc02
Aatif Zaidi <aatifsuntech@gmail.com>
2018-03-03 22:08:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -179,7 +179,7 @@ app.get('/submit-name', function(req,res){
 app.get('/articles/:articleName',function (req, res){
     //Here, articleName = article-one
     
-    pool.query("SELECT * FROM articles WHERE title = "+ req.params.articleName, function (err,result){
+    pool.query("SELECT * FROM articles WHERE title = '"+ req.params.articleName + "'", function (err,result){
     if(err){
         res.status(500).send(err.toString());
     }  else {
