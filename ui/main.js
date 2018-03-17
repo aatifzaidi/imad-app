8332b16066f1773d9e4b272dd648b47738eaa7ec
Aatif Zaidi <aatifsuntech@gmail.com>
2018-03-17 23:38:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,5 @@
-var button = document.getElementById('counter');
 
-button.onclick = function(){
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture the response and store it in a variable 
-    request.onreadystatechange = function(){
-       if(request.readyState === XMLHttpRequest.DONE){
-           
-           //Take some action
-           if(request.status === 200){
-               var counter = request.responseText;
-               var span = document.getElementById('count');
-               span.innerHTML = counter.toString();
-               
-           }
-       } 
-    };
-    
-    //Make the request
-    
-    request.open('GET','http://aatifsuntech.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
-//submit name
+//submit username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
@@ -39,22 +13,25 @@ submit.onclick = function(){
            
            //Take some action
            if(request.status === 200){
-            var names =  request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            for(var i=0;i<names.length;i++){
-             list += '<li>'+names[i]+'</li>';
-    }
-            var ol = document.getElementById('namelist');
-            ol.innerHTML = list;
+            console.log('user logged in');
+            alert('logged in successfully');
+           } 
+           else if(request.status === 403){
+               alert('username/password is incorrect');
+           }
+           else if(request.status === 500){
+               alert('Something went wrong on the Server');
            }
        } 
     };
     
     //Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://aatifsuntech.imad.hasura-app.io/submit-name?name='+name,true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://aatifsuntech.imad.hasura-app.io/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username:username, password:password}));
     
 };
